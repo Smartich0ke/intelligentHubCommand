964c67ef076b6b8d17dfae85d257a6f0514e80fc
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-08-20 08:50:49 +0930

    initial commit

---
+++ src/main/java/com/nikolaipatrick/intelligenthubcommand/IntelligentHubCommand.java
@@ -0,0 +1,17 @@
+package com.nikolaipatrick.intelligenthubcommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IntelligentHubCommand extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
