d0f8843e99e766c9c7b3dd77cadd68f543bb7ee5
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-08-20 08:55:04 +0930

    add enable and disable messages

--- src/main/java/com/nikolaipatrick/intelligenthubcommand/IntelligentHubCommand.java
+++ src/main/java/com/nikolaipatrick/intelligenthubcommand/IntelligentHubCommand.java
@@ -6,12 +6,13 @@ public final class IntelligentHubCommand extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Starting IntelligentHubCommand v1.0.0-SNAPSHOT...");
+        getLogger().info("A Nikolai Patrick plugin - 2022");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Goodbye!");
     }
 }
